9f79c5a6468d8aa237e6843aa6c255d216baf1b7
heniolah <32879482+heniolah@users.noreply.github.com>
2019-04-03 20:18:09 +0300

    Add files via upload

---
+++ Tema3/Tema3/Controllers/MoviesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Tema3.Models;
+
+namespace Tema3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        static List<Movie> movies = new List<Movie>()
+        {
+            new Movie("Title1","Director1"),
+            new Movie("Title2","Director2"),
+            new Movie("Title3","Director3")
+        };
+
+        // GET api/values
+        [HttpGet]
+        public Movie[] Get()
+        {
+            return movies.ToArray();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Movie Get(Guid id)
+        {
+            return movies.FirstOrDefault(Movies => Movies.ID == id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] Movie movie)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            movie.ID = Guid.NewGuid();
+            movies.Add(movie);
+            return Ok();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public IActionResult Put([FromRoute]Guid id, [FromBody] Movie movie)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            Movie dbMovie = movies.FirstOrDefault(Movies => Movies.ID == id);
+            dbMovie.ID = movie.ID;
+            dbMovie.Title = movie.Title;
+            dbMovie.Actors = movie.Actors;
+            dbMovie.Director = movie.Director;
+            dbMovie.Description = dbMovie.Description;
+            dbMovie.Rates = movie.Rates;
+            dbMovie.Price = movie.Price;
+
+            return Ok();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete([FromRoute]Guid id)
+        {
+            movies.Remove(movies.FirstOrDefault(Movies => Movies.ID == id));
+            return Ok();
+        }
+    }
+}
---
+++ Tema3/Tema3/Models/Movies.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tema3.Models
+{
+    public class Movie
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Description { get; set; }
+        public string[] Actors { get; set; }
+        public int[] Rates { get; set; }
+        public int Price { get; set; }
+        List<Movie> movies = new List<Movie>();
+        public Movie(string Title, string Director, string Description = null, string[] Actors = null, int[] Rates = null, int Price = 0)
+        {
+            ID = Guid.NewGuid();
+            this.Title = Title;
+            this.Director = Director;
+            this.Description = Description;
+            this.Actors = Actors;
+            this.Rates = Rates;
+            this.Price = Price;
+        }
+    }
+}
